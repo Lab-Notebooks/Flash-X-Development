4c775fe82dacbd4845837cd0d0ef107290d95e46
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2025-03-11 19:42:04 -0500

    Add a rising bubble simulation

---
+++ simulation/RisingBubble/flashOptions.sh
@@ -0,0 +1,6 @@
+# cache the value of current working directory
+
+FlashSha="faveo/multiphase-precision"
+
+FlashOptions="incompFlow/RisingBubble -auto -maxblocks=400 -2d -nxb=8 -nyb=8 \
+              +amrex +nolwf +serialIO -site=$SiteHome +incomp"
