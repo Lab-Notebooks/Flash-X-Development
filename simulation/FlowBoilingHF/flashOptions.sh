f9bdc1a6d1c890defa8d9e73fa4f5a93f5d5b2ca
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2025-04-17 18:27:40 -0500

    boiling with heat flux sim

---
+++ simulation/FlowBoilingHF/flashOptions.sh
@@ -0,0 +1,10 @@
+# cache the value of current working directory
+
+FlashSha="adhruv/development"
+
+FlashOptions="incompFlow/FlowBoiling -auto -maxblocks=300 -2d -nxb=8 -nyb=8 \
+              +amrex +serialIO +nolwf -site=$SiteHome +incomp -tomlfile=$JobWorkDir/job.input HeaterFluxBC=True --with-unitmods"
+
+if [ $Profile = True ]; then
+	FlashOptions="$FlashOptions +hpctoolkit"
+fi
