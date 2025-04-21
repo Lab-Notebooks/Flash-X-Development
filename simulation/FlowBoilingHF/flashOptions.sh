078adca4f525d69fcd9cbacdffa6cf90a9ad65bd
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2025-04-21 00:19:10 -0500

    update fb hf configuration

--- simulation/FlowBoilingHF/flashOptions.sh
+++ simulation/FlowBoilingHF/flashOptions.sh
@@ -3,7 +3,8 @@
 FlashSha="adhruv/development"
 
 FlashOptions="incompFlow/FlowBoiling -auto -maxblocks=1000 -2d -nxb=8 -nyb=8 \
-              +amrex +serialIO +nolwf -site=$SiteHome +incomp -tomlfile=$JobWorkDir/job.input HeaterFluxBC=True --with-unitmods"
+              +amrex +serialIO +nolwf -site=$SiteHome +incomp \
+              -tomlfile=$JobWorkDir/job.input HeaterFluxBC=True --with-unitmods"
 
 if [ $Profile = True ]; then
 	FlashOptions="$FlashOptions +hpctoolkit"
