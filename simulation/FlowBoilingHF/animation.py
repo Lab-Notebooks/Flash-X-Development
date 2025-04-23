c7f0fab442351a4260a0ff5f3f3d6cc26520f7e5
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2025-04-23 15:31:50 -0500

    add animation script

---
+++ simulation/FlowBoilingHF/animation.py
@@ -0,0 +1,60 @@
+import yt
+import numpy
+from scipy.interpolate import griddata
+from matplotlib import pyplot
+from matplotlib.animation import PillowWriter
+from matplotlib.colors import LogNorm
+
+# Video writer setup
+metadata = dict(title='INS Flow Boiling', artist='Matplotlib', comment='Fluid visualization')
+writer = PillowWriter(fps=10, metadata=metadata)
+
+filetags = [*range(6)]
+
+delta = 0.039
+nx_bins = 2000 #int(2.5/delta)  # Resolution for the interpolation
+ny_bins = 2000 #int(60/delta)
+
+# Set up figure and axis once
+fig, ax = pyplot.subplots(figsize=(10, 2), dpi=300, constrained_layout=True)
+pyplot.rc("font", family="serif", size=15, weight="bold")
+pyplot.rc("axes", labelweight="bold", titleweight="bold")
+pyplot.rc("text", usetex=True)
+
+# Create a blank image that will be updated
+X, Y = None, None
+contourf_plot = [None]  # use list to hold reference
+
+with writer.saving(fig, "INS_Flow_Boiling_Video.gif", dpi=300):
+    for ftag in filetags:
+        filename = f"INS_Flow_Boiling_hdf5_plt_cnt_{str(ftag).zfill(4)}"
+        dataset = yt.load(filename)
+        region = dataset.all_data()
+
+        x = region["x"]
+        y = region["y"]
+
+        if X is None or Y is None:
+            x_bins = numpy.linspace(x.min(), x.max(), nx_bins)
+            y_bins = numpy.linspace(y.min(), y.max(), ny_bins)
+            X, Y = numpy.meshgrid(x_bins, y_bins)
+
+        Z = griddata((x, y), region["dfun"], (X, Y), method='linear')
+
+        ax.clear()
+        contourf = ax.contourf(X, Y, Z, levels=[-0.001, 0.001], extend='both', cmap='Greens')
+        #contourf = ax.contourf(X, Y, Z, levels=numpy.linspace(0.001,10.,100), extend='Greens', cmap='jet', norm = LogNorm())
+        ax.set_xlim([x.min(), x.max()])
+        ax.set_ylim([y.min(), y.max()])
+        ax.set_aspect('equal', adjustable='box')
+        xticks = [0, 30, 60]
+        yticks = [0, 2.5]
+        ax.set_xticks(xticks)
+        ax.set_yticks(yticks)
+	# Set custom labels scaled by 0.7
+        #ax.set_xticklabels([f"{x * 0.7:.1f}" for x in xticks])
+        #ax.set_yticklabels([f"{y * 0.7:.1f}" for y in yticks])
+        #ax.set_xlabel("mm")
+        #ax.set_ylabel("mm")
+        ax.set_title(f"t = {ftag*0.1*8.5:5.1f} ms")
+        writer.grab_frame()
