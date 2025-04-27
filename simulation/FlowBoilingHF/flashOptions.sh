e4da5849dfb73d68ecf8d1d83d6adeb8e6e65b1f
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2025-04-27 14:57:58 -0500

    update flashx tag

--- simulation/FlowBoilingHF/flashOptions.sh
+++ simulation/FlowBoilingHF/flashOptions.sh
@@ -1,6 +1,6 @@
 # cache the value of current working directory
 
-FlashSha="55157877a"
+FlashSha="855ecb71e"
 
 FlashOptions="incompFlow/FlowBoiling -auto -maxblocks=1000 -2d -nxb=16 -nyb=16 \
               +amrex +serialIO +nolwf -site=$SiteHome +incomp \
