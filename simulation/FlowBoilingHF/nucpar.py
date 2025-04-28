332a4280901bb59aac80be1c52569e3f32e1154d
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2025-04-28 14:37:53 -0500

    update nucpar

--- simulation/FlowBoilingHF/nucpar.py
+++ simulation/FlowBoilingHF/nucpar.py
@@ -15,16 +15,30 @@
 #
 # After non-dimensionalization:
 #
-# 1/f = (pi*Nd)/6*(rho'*Re*Pr)/(St*Nu_wall)
+# 1/f = (Rd*3)*(4*pi*Nd)/3*(rho'*Re*Pr)/(St*Nu_wall)
+#
+# The non-dimensional value of Rd is calculated using:
+#
+# Rd = 0.4251*psi*sqrt(2*Bo), where: psi - static contact angle in radians
+#
+# See Equation 39 in:
+#
+# Han Chi-Yeh, Peter Griffith,
+# The mechanism of heat transfer in nucleate pool boiling—Part I: Bubble initiaton, growth and departure,
+# International Journal of Heat and Mass Transfer,
+# Volume 8, Issue 6, 1965, Pages 887-904, ISSN 0017-9310,
+# https://doi.org/10.1016/0017-9310(65)90073-6
+#
+# The growth and wait time can be calculated as:
+#
+# t_growth = (1/4)*(1/f)
+# t_wait   = (3/4)*(1/f)
 #
 # Using empirical assumptions from Equation 44 in:
 #
 # Xiao, L.; Zhuang, Y.; Wu, X.; Yang, J.; Lu, Y.; Liu, Y.;
 # Han, X. A Review of Pool-Boiling Processes Based on Bubble-Dynamics Parameters.
 # Appl. Sci. 2023, 13, 12026. https://doi.org/10.3390/app132112026
-#
-# t_growth = (1/4)*(1/f)
-# t_wait   = (3/4)*(1/f)
 
 import numpy
 import toml
@@ -37,15 +51,17 @@ Re = 1./params["IncompNS"]["ins_invReynolds"]
 Pr = params["HeatAD"]["ht_Prandtl"]
 St = params["Multiphase"]["mph_Stefan"]
 rhoGas = params["Multiphase"]["mph_rhoGas"]
+Bo = params["Multiphase"]["mph_invWeber"]/numpy.sqrt(params["IncompNS"]["ins_gravX"]**2)
+psi = (45./180)*numpy.pi
 
 # Heurisitc assignment of site density
-maxSiteDensity = 7.
+maxSiteDensity = 6.
 
 # Non-dimensional critical heat flux
 heatFluxCHF = 56.
 
 # Set heat flux ratio and compute heat flux
-heatFluxRatio = .70
+heatFluxRatio = .2275
 heatFlux = heatFluxRatio*heatFluxCHF
 
 # Compute 2D and 3D heater areas
@@ -57,8 +73,11 @@ siteDensity = heatFluxRatio*maxSiteDensity
 numSites2D = siteDensity*heaterArea2D
 numSites3D = siteDensity*heaterArea3D
 
+# Calculate bubble departure radius
+Rd = 0.4251*psi*numpy.sqrt(2*Bo)
+
 # Calculation of depature, growth, and wait times
-bubbleFrequency = (St*heatFlux)/(numpy.pi/6)/(siteDensity)/(rhoGas*Re*Pr)
+bubbleFrequency = (St*heatFlux)/(4*(Rd**3)*numpy.pi/3)/(siteDensity)/(rhoGas*Re*Pr)
 nucWaitTime = (3./4)/bubbleFrequency
 nucGrowthTime = (1./4)/bubbleFrequency
 
