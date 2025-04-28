9708a4a687eaabfcc19109cd118e9035ef8aa0a5
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2025-04-28 09:21:12 -0500

    update nucpar

--- simulation/FlowBoilingHF/nucpar.py
+++ simulation/FlowBoilingHF/nucpar.py
@@ -40,35 +40,27 @@ maxSiteDensity = 7.
 # Non-dimensional critical heat flux
 heatFluxCHF = 56.
 
-# Loop over each heater configuration to
-# calculate num sites, and wait time
-for key, info in params["Heater"].items():
+# Set heat flux ratio and compute heat flux
+heatFluxRatio = .70
+heatFlux = heatFluxRatio*heatFluxCHF
 
-    # Only do computations if info is of type dictionary.
-    # This is where heater configuration is stored
-    if type(info) == dict:
+# Compute 2D and 3D heater areas
+heaterArea2D = (params["Grid"]["xmax"]-params["Grid"]["xmin"])
+heaterArea3D = heaterArea2D*(params["Grid"]["zmax"]-params["Grid"]["zmin"])
 
-        # Set heat flux ratio and compute heat flux
-        heatFluxRatio = .70
-        heatFlux = heatFluxRatio*heatFluxCHF
+# Linear scaling for site density to calculate numSites
+siteDensity = heatFluxRatio*maxSiteDensity
+numSites2D = siteDensity*heaterArea2D
+numSites3D = siteDensity*heaterArea3D
 
-        # Compute 2D and 3D heater areas
-        heaterArea2D = (info["xmax"]-info["xmin"])
-        heaterArea3D = heaterArea2D*(info["zmax"]-info["zmin"])
+# Calculation of depature, growth, and wait times
+bubbleFrequency = (St*heatFlux)/(numpy.pi/6)/(siteDensity)/(rhoGas*Re*Pr)
+nucWaitTime = (3./4)/bubbleFrequency
+nucGrowthTime = (1./4)/bubbleFrequency
 
-        # Linear scaling for site density to calculate numSites
-        siteDensity = heatFluxRatio*maxSiteDensity
-        numSites2D = siteDensity*heaterArea2D
-        numSites3D = siteDensity*heaterArea3D
+print(f"siteDensity: {round(siteDensity,1)}")
+print(f"heatFlux:    {round(heatFlux,1)}")
+print(f"nucWaitTime: {round(nucWaitTime,1)}")
+print(f"numSites2D:  {int(numSites2D)}")
+print(f"numSites3D:  {int(numSites3D)}")
 
-        # Calculation of depature, growth, and wait times
-        bubbleFrequency = (St*heatFlux)/(numpy.pi/6)/(siteDensity)/(rhoGas*Re*Pr)
-        nucWaitTime = (3./4)/bubbleFrequency
-        nucGrowthTime = (1./4)/bubbleFrequency
-
-        print(f"Heater.{key}.siteDensity: {round(siteDensity,1)}")
-        print(f"Heater.{key}.heatFlux:    {round(heatFlux,1)}")
-        print(f"Heater.{key}.nucWaitTime: {round(nucWaitTime,1)}")
-        print(f"Heater.{key}.numSites2D:  {int(numSites2D)}")
-        print(f"Heater.{key}.numSites3D:  {int(numSites3D)}")
-        print("------------------------------------------------")
