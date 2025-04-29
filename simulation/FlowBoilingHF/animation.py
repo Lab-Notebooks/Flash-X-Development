9f9636a1f37f53bbb075c70b41fae0f78e19bf72
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2025-04-28 19:03:39 -0500

    update animation

--- simulation/FlowBoilingHF/animation.py
+++ simulation/FlowBoilingHF/animation.py
@@ -4,12 +4,13 @@ from scipy.interpolate import griddata
 from matplotlib import pyplot
 from matplotlib.animation import PillowWriter
 from matplotlib.colors import LogNorm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Video writer setup
 metadata = dict(title='INS Flow Boiling', artist='Matplotlib', comment='Fluid visualization')
 writer = PillowWriter(fps=5, metadata=metadata)
 
-filetags = [*range(16,17)]
+filetags = [*range(199,200)]
 
 delta = 0.039
 nx_bins = 2000 #int(2.5/delta)  # Resolution for the interpolation
@@ -27,7 +28,7 @@ contourf_plot = [None]  # use list to hold reference
 
 with writer.saving(fig, "INS_Flow_Boiling_Video.gif", dpi=300):
     for ftag in filetags:
-        filename = f"INS_Flow_Boiling_hdf5_plt_cnt_{str(ftag).zfill(4)}"
+        filename = f"case-22.75pct-chf/INS_Flow_Boiling_hdf5_plt_cnt_{str(ftag).zfill(4)}"
         dataset = yt.load(filename)
         region = dataset.all_data()
 
@@ -41,17 +42,17 @@ with writer.saving(fig, "INS_Flow_Boiling_Video.gif", dpi=300):
 
         dfun = griddata((x, y), region["dfun"], (X, Y), method='linear')
         temp = griddata((x, y), region["temp"], (X, Y), method='linear')
-        temp = numpy.where(temp <= 0, 1e-4, temp)
+        temp = numpy.where(temp <= 0, 1e-3, temp)
 
         ax.clear()
 
         contourf = ax.contourf(X, Y, dfun, levels=[-0.001, 0.001], extend='both', cmap='Greens')
-        #contourf = ax.contourf(X, Y, temp, levels=numpy.linspace(1e-4,10.,100), extend='both', cmap='Greens', norm = LogNorm())
-        #contour = ax.contour(X, Y, dfun, levels=[0], linecolor='k', linewidths=0.5)
+        #contourf = ax.contourf(X, Y, temp, levels=numpy.linspace(1e-3,1.,200), extend='both', cmap='rainbow', norm = LogNorm())
+        #contour = ax.contour(X, Y, dfun, levels=[0], colors='k', linewidths=0.5, linestyles='-')
         ax.set_xlim([x.min(), x.max()])
         ax.set_ylim([y.min(), y.max()])
         ax.set_aspect('equal', adjustable='box')
-        xticks = [0, 80, 160]
+        xticks = [0, 80, 161]
         yticks = [0, 7]
         ax.set_xticks(xticks)
         ax.set_yticks(yticks)
@@ -60,6 +61,7 @@ with writer.saving(fig, "INS_Flow_Boiling_Video.gif", dpi=300):
         ax.set_yticklabels([f"{y * 0.7:.1f}" for y in yticks])
         ax.set_xlabel(r"mm")
         ax.set_ylabel(r"mm")
+        #cbar = fig.colorbar(contourf, ax=ax, ticks=[1e-3, 5e-2, 1, 10])
         #ax.set_title(f"time = {ftag*0.1*8.5:5.1f} ms, t_nuc = {0.2*8.5:5.1f} ms")
         #ax.set_title(rf"$t_{{\mathrm{{sim}}}} = {ftag*0.1*8.5:5.1f}\ \mathrm{{ms}},\ t_{{\mathrm{{nuc}}}} = {0.8*8.5:5.1f}\ \mathrm{{ms}}$")
         writer.grab_frame()
